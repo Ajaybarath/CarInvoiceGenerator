83d0ddf058efefb43b1a1c45533e53b403a2ed4d
ajaybarath <ajaybarath3@gmail.com>
2021-09-22 16:47:39 +0530

    Added: test function to calculate total fare for multiple rides

--- src/test/java/com/bridgeLabz/carInvoiceGenerator/InvoiceServiceTest.java
+++ src/test/java/com/bridgeLabz/carInvoiceGenerator/InvoiceServiceTest.java
@@ -23,4 +23,15 @@ public class InvoiceServiceTest {
         double fare = invoiceGenerator.calculateFare(distance, time);
         Assertions.assertEquals(5, fare);
     }
+
+    @Test
+    public void givenMultipleRidesCalculateTotalFare() {
+        InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
+        Ride[] rides = {
+                new Ride(2.0, 5),
+                new Ride(0.1, 1)
+        };
+        double fare = invoiceGenerator.calculateFare(rides);
+        Assertions.assertEquals(30, fare);
+    }
 }
