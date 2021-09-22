09cb9c4e0b368a15b555173721a9e05276448f2e
ajaybarath <ajaybarath3@gmail.com>
2021-09-22 17:36:16 +0530

    Added: enum rideType to clasify ride category

--- src/main/java/com/bridgeLabz/carInvoiceGenerator/Ride.java
+++ src/main/java/com/bridgeLabz/carInvoiceGenerator/Ride.java
@@ -3,10 +3,22 @@ package com.bridgeLabz.carInvoiceGenerator;
 public class Ride {
     private final double distance;
     private final int time;
+    private final RideType rideType;
+
+    public enum RideType{
+        NORMAL, PREMIUM
+    }
 
     public Ride(double distance, int time) {
         this.distance = distance;
         this.time = time;
+        this.rideType = RideType.NORMAL;
+    }
+
+    public Ride(double distance, int time, RideType rideType) {
+        this.distance = distance;
+        this.time = time;
+        this.rideType = rideType;
     }
 
     public double getDistance() {
@@ -15,5 +27,9 @@ public class Ride {
 
     public int getTime() {
         return time;
+    }
+
+    public RideType getRideType() {
+        return rideType;
     }
 }
